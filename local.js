c4e7d6551d52ec4d140a5ed66523f8db9970a480
Rajkumar Gunasekaran <gmkrajkumar@gmail.com>
2023-10-04 10:14:29 +0530

    moved index.html outside

--- local.js
+++ local.js
@@ -4,18 +4,28 @@ const path = require('path');
 const httpStatus = require('http-status-codes');
 
 const port = 3000;
-const viewsDirectory = 'views';
+const assetsDirectory = 'assets'; // CSS files will be loaded from here
 
 const server = http.createServer((request, response) => {
     const url = request.url === '/' ? '/index.html' : request.url; // Default to index.html for root URL
-    const filePath = path.join(__dirname, viewsDirectory, url);
+    const filePath = path.join(__dirname, url);
 
     fs.readFile(filePath, (error, data) => {
         if (error) {
-            response.writeHead(httpStatus.NOT_FOUND, { 'Content-Type': 'text/html' });
-            response.end('<h1>File Not Found</h1>');
+            if (error.code === 'ENOENT') {
+                response.writeHead(httpStatus.NOT_FOUND, { 'Content-Type': 'text/html' });
+                response.end('<h1>File Not Found</h1>');
+            } else {
+                response.writeHead(httpStatus.INTERNAL_SERVER_ERROR, { 'Content-Type': 'text/html' });
+                response.end('<h1>Internal Server Error</h1>');
+            }
         } else {
-            response.writeHead(httpStatus.OK, { 'Content-Type': 'text/html' });
+            let contentType = 'text/html';
+            if (filePath.endsWith('.css')) {
+                contentType = 'text/css';
+            }
+
+            response.writeHead(httpStatus.OK, { 'Content-Type': contentType });
             response.write(data);
             response.end();
         }
@@ -23,5 +33,5 @@ const server = http.createServer((request, response) => {
 });
 
 server.listen(port, () => {
-    console.log('The server is listening on port: ${port}');
+    console.log(`The server is listening on port: ${port}`);
 });
